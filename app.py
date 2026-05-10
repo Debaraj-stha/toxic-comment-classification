26d065d0d44375bcfb066e83e68a866ae25f902e
Debaraj-stha <devraj@gmail.com>
2026-05-10 08:03:41 +0545

    bug fixed

--- app.py
+++ app.py
@@ -1,25 +1,31 @@
 from flask import Flask,request,jsonify
 import pandas as pd
 from helper import make_predict
-
+from flask_cors import CORS
+import json
 app = Flask(__name__)
+CORS(app, resources={r"/predict": {"origins": ["http://localhost:3000","https://next-blog-dun.vercel.app"]}},
+      supports_credentials=True,  
+     )
+
 
 @app.route("/")
 def home():
     return "Hello, Flask!"
 
-@app.route("/predict",method=["POST"])
+@app.route("/predict",methods=["POST"])
 def predict():
     data=request.get_json()
-    if "comment" not in data:
+    if "comments" not in data:
         return jsonify({"error": "No comment provided"}), 400
-    comments=data["comment"]
-    if not isinstance(comments, list):
+    comments=data["comments"]
+    parsedComments=json.loads(comments)
+    if not isinstance(parsedComments, list):
         return jsonify({"error": "comments must be a list"}), 400
     
-    predictions=make_predict(comments)
+    predictions=make_predict(parsedComments)
     return jsonify({
-        "predictions": predictions.tolist()
+        "predictions": predictions
     })
 
 if __name__ == "__main__":
--- helper.py
+++ helper.py
@@ -6,7 +6,10 @@ from functools import cache
 import pandas as pd
 import numpy as np
 from deep_translator import GoogleTranslator
-
+import pickle
+from tensorflow.keras.models import load_model
+import os
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 @cache
 def load_spacy():
     try:
@@ -147,44 +150,90 @@ def preprocess(text):
 
     return " ".join(tokens)
 
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 @cache
-def load_model():
-    pass
+def load_my_model():
+    try:
+        model_path = os.path.join(BASE_DIR, "model/toxicity_model.keras")
+        config_path = os.path.join(BASE_DIR, "model/config.pkl")
+        tokenizer_path = os.path.join(BASE_DIR, "model/tokenizer.pkl")
+
+        model = load_model(model_path)
+
+        with open(config_path, "rb") as f:
+            config = pickle.load(f)
+
+        with open(tokenizer_path, "rb") as f:
+            tokenizer = pickle.load(f)
+
+        return model, tokenizer, config
+
+    except FileNotFoundError:
+        raise RuntimeError(" Model files not found. Check paths.")
+    
 
 labels=["","",""]
 
 def make_predict(comments):
-    df=pd.DataFrame(comments,columns=["comment_text"])
+    if not len(comments)>0:
+        return []
+    # df=pd.DataFrame(comments,columns=["comment_text"])
 
-    rep_features = df["comment_text"].apply(repetition_features)
-    df["comment_text"]=df["comment_text"].apply(translate)
-    df["comment_text"] = df["comment_text"].apply(preprocess)
-    df["char_len"] = df["comment_text"].str.len()
-    df["word_len"] = df["comment_text"].str.split().str.len()
+    # rep_features = df["comment_text"].apply(repetition_features)
+    # df["comment_text"]=df["comment_text"].apply(translate)
+    # df["comment_text"] = df["comment_text"].apply(preprocess)
+    # df["char_len"] = df["comment_text"].str.len()
+    # df["word_len"] = df["comment_text"].str.split().str.len()
 
-    bad_features = df["comment_text"].apply(bad_word_features)
-    bad_df = pd.DataFrame(bad_features.tolist())
-    rep_df = pd.DataFrame(rep_features.tolist())
+    # bad_features = df["comment_text"].apply(bad_word_features)
+    # bad_df = pd.DataFrame(bad_features.tolist())
+    # rep_df = pd.DataFrame(rep_features.tolist())
 
 
-    df["has_emoji"] = df["comment_text"].apply(has_emoji)
-    emoji_sentiment=df["comment_text"].apply(emoji_sentiment_multi)
-    emoji_sentiment_df=pd.DataFrame(emoji_sentiment.to_list())
+    # df["has_emoji"] = df["comment_text"].apply(has_emoji)
+    # emoji_sentiment=df["comment_text"].apply(emoji_sentiment_multi)
+    # emoji_sentiment_df=pd.DataFrame(emoji_sentiment.to_list())
 
-    final_df = pd.concat([
-        df.reset_index(drop=True),
-        bad_df.reset_index(drop=True),
-        rep_df.reset_index(drop=True),
-        emoji_sentiment_df.reset_index(drop=True)
-    ], axis=1)
+    # final_df = pd.concat([
+    #     df.reset_index(drop=True),
+    #     bad_df.reset_index(drop=True),
+    #     rep_df.reset_index(drop=True),
+    #     emoji_sentiment_df.reset_index(drop=True)
+    # ], axis=1)
+    model, tokenizer, config = load_my_model()
 
-    vectorizer,model=load_model()
-    comment_text=final_df.loc[:,["comment_text"]]
-    x=final_df.drop(columns=[labels]+["comment_text"])
-    # y=final_test_df[labels]
-    vector=vectorizer.transform(comment_text)
-    vectorized_df=np.hstack([vector,x])
-    y_pred=model.predict(vectorized_df)
-    return y_pred
+    max_len = config.get("max_len")
+    labels = config.get("labels")
 
+    
+    X_text = tokenizer.texts_to_sequences(comments)
+    X_text = pad_sequences(X_text, maxlen=max_len, padding="post")
+
+    y_pred = model.predict(X_text)
+
+    thresholds = [0.5, 0.4, 0.4]
+
+    # Convert probabilities → binary
+    y_pred_bin = (y_pred > thresholds).astype(int)
+
+    # Convert binary → labels
+    labeled_predictions = []
+
+    for i in range(len(y_pred_bin)):
+        current_labels = []
+
+        for j in range(len(labels)):
+            if y_pred_bin[i][j] == 1:
+                current_labels.append(labels[j])
+
+        if not current_labels:
+            current_labels.append("clean")
+
+        labeled_predictions.append(current_labels)
+
+    return labeled_predictions
+                
   
